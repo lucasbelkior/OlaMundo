405b9db3f6213cc2d5ba75aad83866d04ba51951
Lucas Belkior <lucasbelkior9@gmail.com>
2023-08-01 09:20:48 -0300

    estudo das variaveis

--- variavel.py
+++ variavel.py
@@ -1,5 +1,48 @@
 nome = input("Qual é o seu nome?")
 
-print("Ola,",nome,"tudo bem com voce?")
+#print("Ola,",nome,"tudo bem com voce?")
+#print(type(nome))
 
-print("Alteraçao depois do clone")
+a = 10
+b = 5.8
+c = "Rio de Janeiro"
+d= True
+
+print("a: ", a)
+print("b: ", b)
+print("c: ", c)
+print("d: ", d)
+
+print("Tipo da var a:",type(a)) # tipo: inteiro (numeros inteiros)
+print("Tipo da var b:",type(b))# tipo: float (numeros reais)
+print("Tipo da var c:",type(c))# tipo: string (alfanumerico)
+print("Tipo da var d:",type(d))# tipo: booleam (True or False)
+
+a = 20
+print("a: ", a)
+
+b = "Sao Paulo"
+print("b: ", b)
+print("Tipo da var a:",type(a))
+print("Tipo da var b:",type(b))
+
+a= input("Informe um numero:")
+b= input("Informe outro numero:")
+print("a: ", a, "b:",b)
+print("Tipo da var a:",type(a))
+print("Tipo da var b:",type(b))
+c= a + b # concatenou as strings de a e b
+print("c: ", c)
+print("Tipo da var c:",type(c))
+d=int(a)
+print("d: ", d)
+print("Tipo da var d:",type(d))
+
+a= int(input("Informe um numero:"))
+b= int(input("Informe outro numero:"))
+print("a: ", a, "b:",b)
+print("Tipo da var a:",type(a))
+print("Tipo da var b:",type(b))
+c= a + b
+print("c: ", c)
+print("Tipo da var c:",type(c))
